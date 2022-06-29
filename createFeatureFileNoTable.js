905c3c7974f65f6bbd260bde7c4d9c2407ec1b1d
Kiran Adhikari <kiranadh1452@gmail.com>
2022-06-29 11:58:37 +0545

    Function to create content with no table added

--- createFeatureFileNoTable.js
+++ createFeatureFileNoTable.js
@@ -2,8 +2,6 @@
 require("dotenv").config();
 const fs = require("fs");
 
-let title = "Feature: Create a feature file with no tables\n\n";
-let scenarioTitle = "\tScenario: Create a feature file with no tables\n\n";
 let requiredSteps = 100;
 
 // check whether the environment variable 'EACH_STEPS' is set to some value
@@ -16,6 +14,8 @@ if (process.env.EACH_STEPS) {
  */
 const createContentWithNoTable = () => {
     let body = "";
+    let title = "Feature: Create a feature file with no tables\n\n";
+    let scenarioTitle = "\tScenario: Create a feature file with no tables\n\n";
 
     for (i = 0; i < requiredSteps; i++) {
         body += `\t\tGiven here 'Step ${i + 1}'\n`;
@@ -33,4 +33,27 @@ const createContentWithNoTable = () => {
     );
 };
 
+const createContentWithTable = () => {
+    let body = ""; // body of the feature file
+    let title = "Feature: Create a feature file that uses example table\n\n";
+    let scenarioTitle = "\tScenario: With tables\n\n";
+    body += `\t\tGiven here '<variable-name>'\n\t\tWhen here '<variable-name>'\n\t\tThen here '<variable-name>'\n`;
+    let exampleTitle = `\t\tExamples:\n\t\t\t| <variable-name> |\n`;
+    let exampleBody = "";
+
+    for (i = 0; i < requiredSteps; i++) {
+        exampleBody += `\t\t\t| Step ${i + 1} |\n`;
+    }
+
+    fs.writeFile(
+        "features/tables.feature",
+        title + scenarioTitle + body + exampleTitle + exampleBody,
+        (err) => {
+            if (err) throw err;
+            console.log("Saved!");
+        }
+    );
+};
+
+createContentWithTable();
 createContentWithNoTable();
