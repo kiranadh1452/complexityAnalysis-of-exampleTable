8f68ea64cac37027069fb0b73c9776d86e376b6a
Kiran Adhikari <kiranadh1452@gmail.com>
2022-06-29 11:26:11 +0545

    Adjusted stepDef and env var for step calculations

--- createFeatureFileNoTable.js
+++ createFeatureFileNoTable.js
@@ -1,26 +1,32 @@
 // write feature file such that there is no table
+require("dotenv").config();
 const fs = require("fs");
 
-let title = "Feature: Create a feature file with no tables\n\n";
+let title = "\n\nFeature: Create a feature file with no tables\n\n";
 let scenarioTitle = "\tScenario: Create a feature file with no tables\n\n";
 let requiredSteps = 100;
-let body = "";
 
-for(i = 0; i < requiredSteps; i++) {
-    body += `\t\tGiven here 'Step ${i+1}'\n`;
-}
-for(i = 0; i < requiredSteps; i++) {
-    body += `\t\tWhen here 'Step ${i+1}'\n`;
-}
-for(i = 0; i < requiredSteps; i++) {
-    body += `\t\tThen here 'Step ${i+1}'\n`;
+if (process.env.EACH_STEPS) {
+    requiredSteps = process.env.EACH_STEPS;
 }
 
-fs.appendFile(
-    "features/noTables.feature",
-    title + scenarioTitle + body,
-    (err) => {
-        if (err) throw err;
-        console.log("Saved!");
+const createContentWithNoTable = () => {
+    let body = "";
+
+    for (i = 0; i < requiredSteps; i++) {
+        body += `\t\tGiven here 'Step ${i + 1}'\n`;
+        body += `\t\tWhen here 'Step ${i + 1}'\n`;
+        body += `\t\tThen here 'Step ${i + 1}'\n`;
     }
-);
+
+    fs.appendFile(
+        "features/noTables.feature",
+        title + scenarioTitle + body,
+        (err) => {
+            if (err) throw err;
+            console.log("Saved!");
+        }
+    );
+};
+
+createContentWithNoTable();
--- stepDefinitions/testContext.js
+++ stepDefinitions/testContext.js
@@ -4,10 +4,12 @@ Given('here {string}', function (string) {
   // code here
 });
 
-When('here {string}', function (string) {
-  // code here
-});
+// When('here {string}', function (string) {
+//   // code here
+// });
 
-Then('here {string}', function (string) {
-  // code here
-});
+// Then('here {string}', function (string) {
+//   // code here
+// });
+
+// `Given step 1`, `When step 1` and `Then step 1` are handled by a same function.
