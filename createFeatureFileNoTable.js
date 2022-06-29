e326c7876ab01d5928fd362bafb0e319e1c11369
Kiran Adhikari <kiranadh1452@gmail.com>
2022-06-29 11:28:56 +0545

    Code refactored - styling

--- createFeatureFileNoTable.js
+++ createFeatureFileNoTable.js
@@ -2,14 +2,18 @@
 require("dotenv").config();
 const fs = require("fs");
 
-let title = "\n\nFeature: Create a feature file with no tables\n\n";
+let title = "Feature: Create a feature file with no tables\n\n";
 let scenarioTitle = "\tScenario: Create a feature file with no tables\n\n";
 let requiredSteps = 100;
 
+// check whether the environment variable 'EACH_STEPS' is set to some value
 if (process.env.EACH_STEPS) {
     requiredSteps = process.env.EACH_STEPS;
 }
 
+/**
+ * Function to create the content for feature file 'noTables.feature' and write it to the file system.
+ */
 const createContentWithNoTable = () => {
     let body = "";
 
@@ -19,7 +23,7 @@ const createContentWithNoTable = () => {
         body += `\t\tThen here 'Step ${i + 1}'\n`;
     }
 
-    fs.appendFile(
+    fs.writeFile(
         "features/noTables.feature",
         title + scenarioTitle + body,
         (err) => {
--- stepDefinitions/testContext.js
+++ stepDefinitions/testContext.js
@@ -1,15 +1,7 @@
 const { Given, When, Then } = require("@cucumber/cucumber");
 
-Given('here {string}', function (string) {
-  // code here
+Given("here {string}", function (string) {
+    // code here
 });
-
-// When('here {string}', function (string) {
-//   // code here
-// });
-
-// Then('here {string}', function (string) {
-//   // code here
-// });
 
 // `Given step 1`, `When step 1` and `Then step 1` are handled by a same function.
